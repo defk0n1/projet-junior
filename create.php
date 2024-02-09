11f6cd58e7aeb2b1740b7307e0a42f41e7bb899a
unknown <nour.krichen@supcom.tn>
2024-02-09 15:41:15 +0100

    insert

--- create.php
+++ create.php
@@ -2,6 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $image = $_POST["image"];
+    $brand = $_POST["brand"];
     $condition = $_POST["condition"];
     $mileage = $_POST["mileage"];
     $price = $_POST["price"];
@@ -9,13 +10,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!$conn) {
         die("Connection error: " . mysqli_connect_error());
     }
-    $sql = "INSERT INTO moto (name, image, `condition`, mileage, price)
-        VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO moto (name, image, brand, `condition`, mileage, price)
+        VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if(! mysqli_stmt_prepare($stmt, $sql)){
         die(mysqli_error($conn));
     }
-    mysqli_stmt_bind_param($stmt, "sssid", $name, $image, $condition, $mileage, $price );
+    mysqli_stmt_bind_param($stmt, "ssssid", $name, $image, $brand, $condition, $mileage, $price );
     if (!mysqli_stmt_execute($stmt)) {
         die("Error executing statement: " . mysqli_stmt_error($stmt));
     }
@@ -41,10 +42,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" id="name" name="name" required>
         <label for="image">Product Image Link</label> <br>
         <input type="text" id="image" name="image">
+        <label for="brand">Product Brand</label>
+        <select name="brand" id="brand">
+            <option value="BMW">BMW</option>
+            <option value="Harley-Davidson">Harley-Davidson</option>
+            <option value="HONDA">HONDA</option>
+            <option value="SUZUKI">SUZUKI</option>
+            <option value="KAWASAKI">KAWASAKI</option>
+            <option value="YAMAHA">YAMAHA</option>
+        </select>
         <label for="condition">Condition</label>
         <select name="condition" id="condition">
-            <option value="0">Used</option>
-            <option value="1">New</option>
+            <option value="used">Used</option>
+            <option value="New">New</option>
         </select>
         <label for="mileage">Product Mileage</label> <br>
         <input type="number" id="mileage" name="mileage" required>
